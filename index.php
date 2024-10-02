e870bf74a827aa9e35d586575441d9e689f65746
JulianIgelsboeckBS <julian.igelsboeck@students.bs-linz2.ac.at>
2024-10-02 08:41:01 +0200

    Add Cards

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0,maximum-scale=1">
 
     <title>Movie Review</title>
-<?php require_once("DbConn.php");
+    <?php require_once("DbConn.php");
     ?>
 
     <?php include __DIR__ . '/head.php'; ?>
@@ -17,166 +18,176 @@
 <body>
 
 
-<div id="site-content">
-    <?php include __DIR__ . '/nav.php'; ?>
-    <main class="main-content">
-        <div class="container">
-            <div class="page">
-                <div class="row">
-                    <?php include __DIR__ . '/searchFilter.php'; ?>
+    <div id="site-content">
+        <?php include __DIR__ . '/nav.php'; ?>
+        <main class="main-content">
+            <div class="container">
+                <div class="page">
+                    <div class="row">
+                        <?php include __DIR__ . '/searchFilterCollaps.php'; ?>
 
-                    <div class="panel-group">
-                    <div class="panel panel-default">
-                        <div class="panel-heading">
-                            <h4 class="panel-title">
-                                <a data-bs-toggle="collapse" href="#collapse1">Filter anzeigen</a>
-                            </h4>
-                        </div>
-                        <div id="collapse1" class="panel-collapse collapse">
-                            <ul class="list-group">
-                                <li class="list-group-item"><select class="col-md-auto" id="select">
-                                        <option value="" selected>Kategorie</option>
-                                        <option value="Action">Action</option>
-                                        <option value="Drama">Drama</option>
-                                        <option value="Fantasy">Fantasy</option>
 
-                                    </select></li>
-                                <li class="list-group-item">Two</li>
-                                <li class="list-group-item">Three</li>
-                            </ul>
-                            <div class="panel-footer">Footer</div>
-                        </div>
-                    </div>
-                </div>
-                </div>
-                <div class="row">
-                    <div class="col-md-9">
-                        <div class="slider">
-                            <ul class="slides">
-                                <li><a href="#"><img src="dummy/slide-1.jpg" alt="Slide 1"></a></li>
-                                <li><a href="#"><img src="dummy/slide-2.jpg" alt="Slide 2"></a></li>
-                                <li><a href="#"><img src="dummy/slide-3.jpg" alt="Slide 3"></a></li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="col-md-3">
                         <div class="row">
-                            <div class="col-sm-6 col-md-12">
-                                <div class="latest-movie">
-                                    <a href="#"><img src="dummy/thumb-1.jpg" alt="Movie 1"></a>
+                            <div class="col-md-9">
+                                <div class="slider">
+                                    <ul class="slides">
+                                        <li><a href="#"><img src="dummy/slide-1.jpg" alt="Slide 1"></a></li>
+                                        <li><a href="#"><img src="dummy/slide-2.jpg" alt="Slide 2"></a></li>
+                                        <li><a href="#"><img src="dummy/slide-3.jpg" alt="Slide 3"></a></li>
+                                    </ul>
                                 </div>
                             </div>
-                            <div class="col-sm-6 col-md-12">
+                            <div class="col-md-3">
+                                <div class="row">
+                                    <div class="col-sm-6 col-md-12">
+                                        <div class="latest-movie">
+                                            <a href="#"><img src="dummy/thumb-1.jpg" alt="Movie 1"></a>
+                                        </div>
+                                    </div>
+                                    <div class="col-sm-6 col-md-12">
+                                        <div class="latest-movie">
+                                            <a href="#"><img src="dummy/thumb-2.jpg" alt="Movie 2"></a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div> <!-- .row -->
+
+                        <div class="row">
+                            <div class="col-sm-6 col-md-3">
                                 <div class="latest-movie">
-                                    <a href="#"><img src="dummy/thumb-2.jpg" alt="Movie 2"></a>
+                                    <a href="#"><img src="dummy/thumb-3.jpg" alt="Movie 3"></a>
+                                    <div class="movie-details">
+                                        <p><strong>Filmname:</strong> Movie 3</p>
+                                        <p><strong>Genre:</strong> Action</p>
+                                        <p><strong>Erscheinungsjahr:</strong> 2023</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-sm-6 col-md-3">
+                                <div class="latest-movie">
+                                    <a href="#"><img src="dummy/thumb-4.jpg" alt="Movie 4"></a>
+                                    <div class="movie-details">
+                                        <p><strong>Filmname:</strong> Movie 4</p>
+                                        <p><strong>Genre:</strong> Drama</p>
+                                        <p><strong>Erscheinungsjahr:</strong> 2022</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-sm-6 col-md-3">
+                                <div class="latest-movie">
+                                    <a href="#"><img src="dummy/thumb-5.jpg" alt="Movie 5"></a>
+                                    <div class="movie-details">
+                                        <p><strong>Filmname:</strong> Movie 5</p>
+                                        <p><strong>Genre:</strong> Comedy</p>
+                                        <p><strong>Erscheinungsjahr:</strong> 2024</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-sm-6 col-md-3">
+                                <div class="latest-movie">
+                                    <a href="#"><img src="dummy/thumb-6.jpg" alt="Movie 6"></a>
+                                    <div class="movie-details">
+                                        <p><strong>Filmname:</strong> Movie 6</p>
+                                        <p><strong>Genre:</strong> Sci-Fi</p>
+                                        <p><strong>Erscheinungsjahr:</strong> 2021</p>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                </div> <!-- .row -->
-                <div class="row">
-                    <div class="col-sm-6 col-md-3">
-                        <div class="latest-movie">
-                            <a href="#"><img src="dummy/thumb-3.jpg" alt="Movie 3"></a>
-                        </div>
-                    </div>
-                    <div class="col-sm-6 col-md-3">
-                        <div class="latest-movie">
-                            <a href="#"><img src="dummy/thumb-4.jpg" alt="Movie 4"></a>
-                        </div>
-                    </div>
-                    <div class="col-sm-6 col-md-3">
-                        <div class="latest-movie">
-                            <a href="#"><img src="dummy/thumb-5.jpg" alt="Movie 5"></a>
-                        </div>
-                    </div>
-                    <div class="col-sm-6 col-md-3">
-                        <div class="latest-movie">
-                            <a href="#"><img src="dummy/thumb-6.jpg" alt="Movie 6"></a>
-                        </div>
-                    </div>
-                </div> <!-- .row -->
 
-                <div class="row">
-                    <div class="col-md-4">
-                        <h2 class="section-title">December premiere</h2>
-                        <p>Lorem ipsum dolor sit amet consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
-                        <ul class="movie-schedule">
-                            <li>
-                                <div class="date">16/12</div>
-                                <h2 class="entry-title"><a href="#">Perspiciatis unde omnis</a></h2>
-                            </li>
-                            <li>
-                                <div class="date">16/12</div>
-                                <h2 class="entry-title"><a href="#">Perspiciatis unde omnis</a></h2>
-                            </li>
-                            <li>
-                                <div class="date">16/12</div>
-                                <h2 class="entry-title"><a href="#">Perspiciatis unde omnis</a></h2>
-                            </li>
-                            <li>
-                                <div class="date">16/12</div>
-                                <h2 class="entry-title"><a href="#">Perspiciatis unde omnis</a></h2>
-                            </li>
-                        </ul> <!-- .movie-schedule -->
-                    </div>
-                    <div class="col-md-4">
-                        <h2 class="section-title">November premiere</h2>
-                        <p>Lorem ipsum dolor sit amet consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
-                        <ul class="movie-schedule">
-                            <li>
-                                <div class="date">16/12</div>
-                                <h2 class="entry-title"><a href="#">Perspiciatis unde omnis</a></h2>
-                            </li>
-                            <li>
-                                <div class="date">16/12</div>
-                                <h2 class="entry-title"><a href="#">Perspiciatis unde omnis</a></h2>
-                            </li>
-                            <li>
-                                <div class="date">16/12</div>
-                                <h2 class="entry-title"><a href="#">Perspiciatis unde omnis</a></h2>
-                            </li>
-                            <li>
-                                <div class="date">16/12</div>
-                                <h2 class="entry-title"><a href="#">Perspiciatis unde omnis</a></h2>
-                            </li>
-                        </ul> <!-- .movie-schedule -->
-                    </div>
-                    <div class="col-md-4">
-                        <h2 class="section-title">October premiere</h2>
-                        <p>Lorem ipsum dolor sit amet consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
-                        <ul class="movie-schedule">
-                            <li>
-                                <div class="date">16/12</div>
-                                <h2 class="entry-title"><a href="#">Perspiciatis unde omnis</a></h2>
-                            </li>
-                            <li>
-                                <div class="date">16/12</div>
-                                <h2 class="entry-title"><a href="#">Perspiciatis unde omnis</a></h2>
-                            </li>
-                            <li>
-                                <div class="date">16/12</div>
-                                <h2 class="entry-title"><a href="#">Perspiciatis unde omnis</a></h2>
-                            </li>
-                            <li>
-                                <div class="date">16/12</div>
-                                <h2 class="entry-title"><a href="#">Perspiciatis unde omnis</a></h2>
-                            </li>
-                        </ul> <!-- .movie-schedule -->
-                    </div>
-                </div>
-            </div>
-        </div> <!-- .container -->
-    </main>
-    <?php include __DIR__ . '/footer.php'; ?>
-</div>
-<!-- Default snippet for navigation -->
+                        <div class="row">
+                            <div class="col-sm-6 col-md-3">
+                                <div class="card mb-4 shadow-sm">
+                                    <a href="#">
+                                        <img src="dummy/thumb-3.jpg" class="card-img-top" alt="Movie 3">
+                                    </a>
+                                    <div class="card-body text-center p-3">
+                                        <h5 class="card-title mt-2">Movie 3</h5>
+                                        <p class="card-text"><strong>Genre:</strong> Action</p>
+                                        <p class="card-text"><strong>Erscheinungsjahr:</strong> 2023</p>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-sm-6 col-md-3">
+                                <div class="card mb-4 shadow-sm">
+                                    <a href="#">
+                                        <img src="dummy/thumb-4.jpg" class="card-img-top" alt="Movie 4">
+                                    </a>
+                                    <div class="card-body text-center p-3">
+                                        <h5 class="card-title mt-2">Movie 4</h5>
+                                        <p class="card-text"><strong>Genre:</strong> Drama</p>
+                                        <p class="card-text"><strong>Erscheinungsjahr:</strong> 2022</p>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-sm-6 col-md-3">
+                                <div class="card mb-4 shadow-sm">
+                                    <a href="#">
+                                        <img src="dummy/thumb-5.jpg" class="card-img-top" alt="Movie 5">
+                                    </a>
+                                    <div class="card-body text-center p-3">
+                                        <h5 class="card-title mt-2">Movie 5</h5>
+                                        <p class="card-text"><strong>Genre:</strong> Comedy</p>
+                                        <p class="card-text"><strong>Erscheinungsjahr:</strong> 2024</p>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-sm-6 col-md-3">
+                                <div class="card mb-4 shadow-sm">
+                                    <a href="#">
+                                        <img src="dummy/thumb-6.jpg" class="card-img-top" alt="Movie 6">
+                                    </a>
+                                    <div class="card-body text-center p-3">
+                                        <h5 class="card-title mt-2">Movie 6</h5>
+                                        <p class="card-text"><strong>Genre:</strong> Sci-Fi</p>
+                                        <p class="card-text"><strong>Erscheinungsjahr:</strong> 2021</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
 
 
-<script src="disableFilter.js"></script>
-<script src="js/jquery-1.11.1.min.js"></script>
-<script src="js/plugins.js"></script>
-<script src="js/app.js"></script>
+
+                        <div class="row">
+                            <div class="col-sm-6 col-md-3">
+                                <div class="latest-movie">
+                                    <a href="#"><img src="dummy/thumb-3.jpg" alt="Movie 3"></a>
+                                </div>
+                            </div>
+                            <div class="col-sm-6 col-md-3">
+                                <div class="latest-movie">
+                                    <a href="#"><img src="dummy/thumb-4.jpg" alt="Movie 4"></a>
+                                </div>
+                            </div>
+                            <div class="col-sm-6 col-md-3">
+                                <div class="latest-movie">
+                                    <a href="#"><img src="dummy/thumb-5.jpg" alt="Movie 5"></a>
+                                </div>
+                            </div>
+                            <div class="col-sm-6 col-md-3">
+                                <div class="latest-movie">
+                                    <a href="#"><img src="dummy/thumb-6.jpg" alt="Movie 6"></a>
+                                </div>
+                            </div>
+                        </div> <!-- .row -->
+
+
+                    </div>
+                </div> <!-- .container -->
+        </main>
+        <?php include __DIR__ . '/footer.php'; ?>
+    </div>
+    <!-- Default snippet for navigation -->
+
+
+    <script src="disableFilter.js"></script>
+    <script src="js/jquery-1.11.1.min.js"></script>
+    <script src="js/plugins.js"></script>
+    <script src="js/app.js"></script>
 
 </body>
 
---
+++ searchFilterCollaps.php
@@ -0,0 +1,40 @@
+<div class="panel-group">
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <h4 class="panel-title">
+                                        <a data-bs-toggle="collapse" href="#collapse1">Suchfilter anzeigen</a>
+                                    </h4>
+                                </div>
+                                <div id="collapse1" class="panel-collapse collapse">
+                                    <ul class="list-group">
+                                        <li class="list-group-item"><select class="col-md-auto" id="select">
+                                                <option value="" selected>Kategorie</option>
+                                                <option value="Action">Action</option>
+                                                <option value="Drama">Drama</option>
+                                                <option value="Fantasy">Fantasy</option>
+
+                                            </select></li>
+                                        <li class="list-group-item"><select class="col-md-auto" id="select">
+                                                <option value="" selected>Jahr</option>
+                                                <option value="2012">2012</option>
+                                                <option value="2013">2013</option>
+                                                <option value="2014">2014</option>
+                                            </select></li>
+                                        <li class="list-group-item"><select class="col-md-auto" id="select">
+                                                <option value="" selected>Bewertung</option>
+                                                <option value="Top">Top</option>
+                                                <option value="Sehr Gut">Sehr Gut</option>
+                                                <option value="Gut">Gut</option>
+                                            </select></li>
+                                        <li class="list-group-item"><select class="col-md-auto" id="select">
+                                                <option value="" selected>Streamingdienst</option>
+                                                <option value="Netflix">Netflix</option>
+                                                <option value="Amazon">Amazon Prime</option>
+                                                <option value="Sky">Sky</option>
+                                            </select></li>
+                                    </ul>
+                                    <div class="panel-footer"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
